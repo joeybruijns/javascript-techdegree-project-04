effeec04c3292d88767da1288de5109c1919b5d8
Joey Bruijns <bruijns.joey@gmail.com>
2019-12-28 22:33:46 +0100

    Add remaining functions to make the app work

--- js/Game.js
+++ js/Game.js
@@ -9,9 +9,36 @@ class Game {
         this.activePhrase = null;
     }
 
+    // ===> PUT ALL VARIABLES IN THE CONSTRUCTOR?????? //
+
+    resetGame() {
+        // remove all li items
+        
+
+        // update and enable all keys to use key class
+        const letters =  document.querySelectorAll('.keyrow > button');
+        for (let i = 0; i < letters.length; i += 1) {
+            letters[i].className = 'key';
+            letters[i].disabled = false;
+        }
+
+        // reset all the heart images (and the counter??)
+        const hearts = document.querySelectorAll('.tries img');
+        for (let i = 0; i < hearts.length; i += 1) {
+            let checkSrc = hearts[i].getAttribute('src');
+            if (checkSrc === 'images/lostHeart.png') {
+                hearts[i].setAttribute('src', 'images/liveHeart.png');
+            }
+        }
+        this.missed = 0;
+    }
+
     startGame() {
+        // reset the game when the start button is clicked
+        this.resetGame();
         document.getElementById('overlay').style.display = 'none';
-        this.activePhrase = this.getRandomPhrase;
+        this.activePhrase = this.getRandomPhrase();
+        console.log(this.activePhrase); //DELETE!!
         this.activePhrase.addPhraseToDisplay();
     }
 
@@ -20,43 +47,85 @@ class Game {
      * @return {array} An array of phrases that could be used in the game
      * */
     createPhrases(...phrase) {
-        this.phrases.push(phrase);
+        this.phrases.push(...phrase);
     };
 
     /**
-     *
-     * @returns {string}
+     * Gets a random phrase from the phrases array
+     * @returns {Phrase} A random phrase from the phrases array
      */
     getRandomPhrase() {
         const randomNumber = Math.floor((Math.random() * this.phrases.length));
-        return this.phrases[randomNumber];
+        const selectedPhrase = this.phrases[randomNumber];
+        return new Phrase(selectedPhrase);
+        //return this.phrases[randomNumber];
     }
 
-    handleInteraction() {
+    handleInteraction(button) {
+        // disable the clicked letter
+        button.disabled = true;
+        // if letter is not in phrase
+        if (!this.activePhrase.checkLetter(button.textContent)) {
+            button.classList.replace('key', 'wrong');
+            this.removeLife();
+        } else {
+            button.classList.replace('key', 'chosen');
+            this.activePhrase.showMatchedLetter(button.textContent);
+            this.checkForWin();
 
+            // if (this.checkForWin) {
+            //     console.log('win!!!');
+            //     //this.gameOver(true);
+            // }
+        }
+        // if player has won, call gameOver();
     }
 
     removeLife() {
+        const allHearts = document.querySelectorAll('.tries');
+        //let heartsIndex = 4;
+        allHearts[this.missed].firstElementChild.setAttribute('src', 'images/lostHeart.png');
+        
+        this.missed += 1;
 
+        if (this.missed === 5) {
+            this.gameOver(false);
+        }
     }
 
     checkForWin() {
         // check all classes from list item if nothing is hidden
         // return either true or false
+        const hiding = document.getElementsByClassName('hide letter');
+        if (hiding.length === 0) {
+            this.gameOver(true);
+        }
+        return false;
 
+        // const ul = document.querySelector('#phrase ul');
+        // const listItems = ul.getElementsByTagName('li');
+        //
+        // for (let i = 0; i < listItems.length; i += 1) {
+        //     if (listItems[i].classList.contains('hide')) {
+        //         return false;
+        //     } else {
+        //         this.gameOver(true);
+        //         console.log('WIN!!');
+        //     }
+        // }
     }
 
-    gameOver() {
+    gameOver(result) {
         const overlay = document.getElementById('overlay');
         const overlayMessage = document.querySelector('#overlay h1');
         overlay.style.display = 'block';
 
-        if (this.checkForWin) {
+        if (result) {
             overlay.setAttribute('class', 'win');
             overlayMessage.textContent = 'Yeah, you win!!!';
         } else {
             overlay.setAttribute('class', 'lose');
-            overlayMessage.textContent = 'Better luck next time..';
+            overlayMessage.textContent = 'Better luck next time...';
         }
     }
 }
--- js/app.js
+++ js/app.js
@@ -2,14 +2,19 @@
  * Project 4 - OOP Game App
  * app.js */
 
+const startGameButton = document.getElementById('btn__reset');
+//const qwertyButtons = document.getElementsByClassName('keyrow');
+const qwertyButtons = document.getElementById('qwerty');
 const game = new Game();
 
 game.createPhrases('dit is een test', 'nog een test', 'de laatste test');
 
-const logPhrase = (phrase) => console.log(`Phrase - phrase: ${phrase}`);
+startGameButton.addEventListener('click', function() {
+    game.startGame();
+});
 
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
+qwertyButtons.addEventListener('click', function(event) {
+    if (event.target.tagName === 'BUTTON') {
+        game.handleInteraction(event.target);
+    }
+});
